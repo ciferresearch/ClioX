61a3b9abbe26d4fcca7854663f2862628596e09b
Chriscodinghard <c.chowwww@gmail.com>
2025-05-16 12:18:32 +0200

    fix:docker image fix

--- core-components/algo/text_sentiment/text_analysis.py
+++ core-components/algo/text_sentiment/text_analysis.py
@@ -148,6 +148,7 @@ def sentiment_classication(df):
     return sentiment_df
 
 def get_job_details():
+    root = os.getenv('ROOT_FOLDER', '')
     """Reads in metadata information about assets used by the algo"""
     job = dict()
     job['dids'] = json.loads(os.getenv('DIDS', None))
@@ -158,25 +159,13 @@ def get_job_details():
     algo_did = os.getenv('TRANSFORMATION_DID', None)
     if job['dids'] is not None:
         for did in job['dids']:
-            # get the ddo from disk
-            filename = '/data/ddos/' + did
-            print(f'Reading json from {filename}')
-            with open(filename) as json_file:
-                ddo = json.load(json_file)
-                # search for metadata service
-                for service in ddo['service']:
-                    if service['type'] == 'metadata':
-                        job['files'][did] = list()
-                        index = 0
-                        for file in service['attributes']['main']['files']:
-                            job['files'][did].append(
-                                '/data/inputs/' + did + '/' + str(index))
-                            index = index + 1
+            job['files'][did] = list()
+            # Just one file for DID with name "0"
+            job['files'][did].append(root + '/data/inputs/' + did + '/0')
     if algo_did is not None:
         job['algo']['did'] = algo_did
-        job['algo']['ddo_path'] = '/data/ddos/' + algo_did
+        job['algo']['ddo_path'] = root + '/data/ddos/' + algo_did
     return job
-
 
 def text_analysis(job_details=None):
     '''
